1cf102f10dc5fbeb1fc457406bad08f44d42cec9
RootHolic <wudi@proqod.com>
2016-11-14 09:45:57 +0800

    dockerize: change yml file to fit docker machine and update gitignore

--- startmachin.sh
+++
@@ -1,5 +0,0 @@
-docker kill $(docker ps -a -q)&&docker rm $(docker ps -a -q)
-docker-compose build
-docker-compose up -d
-docker-compose run web python /project/mysite/manage.py makemigrations
-docker-compose run web python /project/mysite/manage.py migrate --noinput --no-initial-data
